4c5632460b291610db7d2f03595dc67a8b2ec4a8
Omkar Kaul <omkar@xtra.co.nz>
2020-05-30 02:19:53 +1200

    used cors and implemented unimplemented endpoints

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 let recipes = []; // array of receipt objects
 
 
 /** GET ALL RECIPES */
-app.get('/receipe', (req, res) => {
+app.get('/recipe', (req, res) => {
     const data = {
-        "recipes": receipes
+        "recipes": recipes
     };
     
     res.status(200).send(data);
@@ -38,24 +43,40 @@ app.get('/receipe/:id', (req, res) => {
 
 /** CREATE NEW RECEIPE */
 app.post('/recipe', (req, res) => {
-    const data = req.body;
+    let newId;
 
-    // do more things here
+    if (recipes.length > 0) {
+        newId = recipes[recipes.length-1].id + 1; // get last recipe currently in array, newId is that recipe's id + 1
+    } else {
+        newId = 1;
+    }
+    
+    const data = {
+        "id": newId,
+        "name": req.body.name,
+        "content": req.body.content
+    }
+
+    console.log(data);
+    recipes.push(data);
+
+    res.status(200).send("Posted recipe");
 });
 
 /** DELETE A RECIPE */
-app.delete('recipe/:id', (req, res) => {
-    const reqId = req.params.id;
-
+app.delete('/recipe/:id', (req, res) => {
+    const reqId = parseInt(req.params.id);
+    
     for (let i = 0; i < recipes.length; i++) {
-        const recipe = receipes[i];
-
+        const recipe = recipes[i];
+        
         if (recipe.id === reqId) {
             recipes.splice(i, 1); // delete receipe at curr index
         
             res.status(200).send({
                 "message": `Recipe with id ${reqId} has been deleted!`
             });
+            return;
         };
     }
     
